d4ec30b4b89dc64d5ecb3220deb113bc8497a40f
AdrienChew246 <74026675+AdrienChew246@users.noreply.github.com>
2026-03-20 07:24:34 +0000

    Build: ()

--- obs.html/static/obsidian_core.js
+++ obs.html/static/obsidian_core.js
@@ -13,7 +13,7 @@ var CONFIG_CLOSE_LEFT_PANE_IF_EMPTY = 0;
 var RELATIVE_PATHS = 0;
 var documentation_mode = 1;
 var tab_mode = !no_tab_mode;
-var gzip_hash = '2348764309509613763297160389098098186'                       // used to check whether the localStorage data is stale
+var gzip_hash = '66833552801743477740556298320973776008'                       // used to check whether the localStorage data is stale
 
 // global cache
 var fn_cache_ls_available = null;
